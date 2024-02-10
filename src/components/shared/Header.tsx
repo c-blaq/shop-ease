b1f9f5f92b1923e029eba2bbf8390a5745da300c
Fathi <babayejuadesina@gmail.com>
2024-02-10 15:41:26 +0100

    chore: update header tyle

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -25,9 +25,9 @@ const PageHeader = () => {
     },
   ];
   return (
-    <Header className="bg-white/30 max-w-screen-2xl mx-auto rounded-full fixed z-10 left-0 right-0 top-5 backdrop-blur-md">
-      <div className="flex justify-between items-center gap-5">
-        <nav className="flex gap-5 items-center">
+    <Header className="bg-transparent fixed z-10 left-0 right-0 top-5 px-5">
+      <div className="flex justify-between items-center gap-5 bg-white/30 w-full max-w-screen-2xl mx-auto rounded-full backdrop-blur-md py-4 xs:py-0 px-5">
+        <nav className="hidden xs:flex gap-5 items-center">
           {NAV_ITEMS.map((item: INav) => (
             <Link
               key={item.title}
