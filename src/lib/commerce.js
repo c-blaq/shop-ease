8d15508d2cb2c656d36b39a8d1a0baf7eaea9f16
Fathi <babayejuadesina@gmail.com>
2024-02-16 15:46:43 +0100

    feat: instantiate Commerce.js

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,4 +1,3 @@
 import Commerce from "@chec/commerce.js";
 
-
-const client = new Commerce({process.env.NEXT_PUBLIC_CHEC_PUBLIC_API_KEY}, true)
+const client = new Commerce(process.env.NEXT_PUBLIC_CHEC_PUBLIC_API_KEY, true);
