d816924f7b4393a6c2be5272bbbccdff91892b42
Fathi <babayejuadesina@gmail.com>
2024-02-20 00:10:21 +0100

    feat: loader

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -8,6 +8,7 @@ import Link from "next/link";
 import React, { useEffect } from "react";
 import { FaMinus } from "react-icons/fa";
 import { FaChevronLeft, FaDeleteLeft, FaTrash } from "react-icons/fa6";
+import Loader from "@/components/shared/Loader";
 
 const Cart = () => {
   const { cartItems, fetchCart } = useCart();
@@ -27,86 +28,92 @@ const Cart = () => {
         </Link>
       </div>
 
-      <div className="w-full mt-10">
-        <table className="w-full">
-          <thead className="border-t-2 border-b-2">
-            <tr>
-              <th className="py-2 px-2 sm:px-0">Product</th>
-              <th className="py-2 px-2 sm:px-0 hidden sm:block">Price</th>
-              <th className="py-2 px-2 sm:px-0">Quantity</th>
-              <th className="py-2 px-2 sm:px-0">Amount</th>
-            </tr>
-          </thead>
+      {!cartItems.line_items?.length ? (
+        <div className="min-h-[200px] flex justify-center items-center">
+          <Loader />
+        </div>
+      ) : (
+        <div className="w-full mt-10">
+          <table className="w-full">
+            <thead className="border-t-2 border-b-2">
+              <tr>
+                <th className="py-2 px-2 sm:px-0">Product</th>
+                <th className="py-2 px-2 sm:px-0 hidden sm:block">Price</th>
+                <th className="py-2 px-2 sm:px-0">Quantity</th>
+                <th className="py-2 px-2 sm:px-0">Amount</th>
+              </tr>
+            </thead>
 
-          <tbody className="text-center">
-            {cartItems ? (
-              cartItems.line_items?.map((item: LineItem) => (
-                <tr className="border-b-2">
-                  <td className="py-2 px-2 sm:px-0">
-                    <div className="flex flex-col sm:flex-row justify-center items-center gap-2">
-                      <Image
-                        src={item.image?.url}
-                        alt="jacket"
-                        width={50}
-                        height={50}
-                      />
-                      <div>
-                        <h3>Jacket</h3>
+            <tbody className="text-center">
+              {cartItems.line_items?.length ? (
+                cartItems.line_items?.map((item: LineItem) => (
+                  <tr className="border-b-2">
+                    <td className="py-2 px-2 sm:px-0">
+                      <div className="flex flex-col sm:flex-row justify-center items-center gap-2">
+                        <Image
+                          src={item.image?.url}
+                          alt="jacket"
+                          width={50}
+                          height={50}
+                        />
+                        <div>
+                          <h3>Jacket</h3>
+                        </div>
                       </div>
-                    </div>
-                  </td>
-                  <td className="py-2 px-2 sm:px-0 hidden sm:block">
-                    {item.price.formatted_with_symbol}
-                  </td>
-                  <td className="py-2 px-2 sm:px-0">
-                    <div className="flex flex-col sm:flex-row items-center justify-center gap-1">
-                      <Button type="text">-</Button>
-                      <span>{item.quantity}</span>
-                      <Button type="text">+</Button>
-                    </div>
-                  </td>
-                  <td className="py-2 px-2 sm:px-0">
-                    {item.line_total.formatted_with_symbol}{" "}
-                    <span className="block text-gray-400 sm:hidden">
-                      ({item.price.formatted_with_symbol})
-                    </span>
-                  </td>
-                  <td className="py-2 px-2 sm:px-0 w-5">
-                    <Button
-                      type="text"
-                      className="text-black/75 hover:!text-red-500"
-                    >
-                      <FaTrash />
-                    </Button>
-                  </td>
-                </tr>
-              ))
-            ) : (
-              <p>No item</p>
-            )}
-          </tbody>
-        </table>
+                    </td>
+                    <td className="py-2 px-2 sm:px-0 hidden sm:block">
+                      {item.price.formatted_with_symbol}
+                    </td>
+                    <td className="py-2 px-2 sm:px-0">
+                      <div className="flex flex-col sm:flex-row items-center justify-center gap-1">
+                        <Button type="text">-</Button>
+                        <span>{item.quantity}</span>
+                        <Button type="text">+</Button>
+                      </div>
+                    </td>
+                    <td className="py-2 px-2 sm:px-0">
+                      {item.line_total.formatted_with_symbol}{" "}
+                      <span className="block text-gray-400 sm:hidden">
+                        ({item.price.formatted_with_symbol})
+                      </span>
+                    </td>
+                    <td className="py-2 px-2 sm:px-0 w-5">
+                      <Button
+                        type="text"
+                        className="text-black/75 hover:!text-red-500"
+                      >
+                        <FaTrash />
+                      </Button>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <p>No item</p>
+              )}
+            </tbody>
+          </table>
 
-        <div className="mt-5  items-center justify-between">
-          <h2 className="text-xl">
-            Subtotal: {cartItems.subtotal.formatted_with_symbol}
-          </h2>
-          <div className="flex mt-5 gap-5">
-            <Button
-              type="text"
-              className="border border-black rounded-sm flex-1"
-            >
-              Clear
-            </Button>
-            <Link
-              href="/checkout"
-              className="flex flex-1 justify-center items-center px-5 bg-black/95 rounded-sm hover:bg-black/[0.06] hover:text-black text-white"
-            >
-              Checkout
-            </Link>
+          <div className="mt-5  items-center justify-between">
+            <h2 className="text-xl">
+              Subtotal: {cartItems.subtotal?.formatted_with_symbol}
+            </h2>
+            <div className="flex mt-5 gap-5">
+              <Button
+                type="text"
+                className="border border-black rounded-sm flex-1"
+              >
+                Clear
+              </Button>
+              <Link
+                href="/checkout"
+                className="flex flex-1 justify-center items-center px-5 bg-black/95 rounded-sm hover:bg-black/[0.06] hover:text-black text-white"
+              >
+                Checkout
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Spin } from "antd";
+
+const Loader: React.FC = () => <Spin fullscreen />;
+
+export default Loader;
